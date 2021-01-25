2937915060f11a9e8b0056d855c65e3b4de24558
ahmedayman220 <ahmedayman220@users.noreply.github.com>
2021-01-25 16:32:22 +0200

    unifox 1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// // =================== filter product ==============
+// function filter() {
+//     let img_fillter = document.querySelectorAll(".portoflios-item img")
+//     let btns = document.querySelectorAll(".portoflios-button button")
+//     btns.forEach((btn) => {
+//         btn.addEventListener("click" ,  (b) => {
+//           id_btn =  b.currentTarget.getAttribute("id")
+            
+//           if(id_btn == "all") {
+//             console.log("yes");
+//           }else {
+//                 let nameData = img_fillter.forEach((image) => {
+//                    image.getAttribute("data-id")
+//                 }) 
+//                 console.log(nameData);
+//                 // img_fillter.filter((item) => id_btn == nameData)
+//                 // console.log(item);
+//           }
+//         })
+// })
+// }
+// filter()
+
+// // =================== filter product ==============
